1bb1b9ac62ad505dacd1bd86d71334c067009ba5
v4n700 <kiev6225@gmail.com>
2019-04-18 19:46:08 +0300

    AboutUS info

--- app/screens/AboutUsScreen.js
+++ app/screens/AboutUsScreen.js
@@ -18,8 +18,12 @@ export default class AboutUsScreen extends Component{
       <View style={styles.container}>
         <HeaderComponent navigation = {this.props.navigation}/>
         <View style={styles.textView}>
-          <Text style={styles.headerText}>Что такое СТЭК?</Text>
-          <Text style={styles.bodyText}>Тут куча всякого текста всякого текста всякого текста всякого текста всякого текста всякого текста</Text>
+          <Text style={styles.headerText}>Что такое «СТЭК»?</Text>
+          <Text style={styles.bodyText}>«СТЭК» - студенческая электронная карта,к оторая так же заменяет студенческий, читательский и профсоюзный билет.</Text>
+          <Text style={styles.bodyText}>Проект появился в 2004 году в профкоме обучающихся ПетрГУ и успешно реализуется на протяжении 14 лет.</Text>
+          <Text style={styles.bodyText}>Мы охватываем большинство молодежи и студенчества в г. Петрозаводске. В настоящее время держателями карт «СТЭК» являются более 8000 обучающихся ПетрГУ.</Text>
+          <Text style={styles.bodyText}>С каждым годом держателей карт становится все больше!</Text>
+          <Text style={styles.bodyText}>С нами сотрудничают уже более 150 организаций города Петрозаводска, которые предоставляют скидки от 3 до 50 % держателям карт «СТЭК». </Text>
         </View>
         <View style={styles.links}>
           <CircleLinksComponent imagePath={require('../assets/menu/svyaz/vk/drawable-hdpi/Group.png')} link='https://vk.com/card_stec'/>
@@ -53,8 +57,10 @@ const styles = StyleSheet.create({
   bodyText: {
     fontFamily: 'Roboto',
     marginLeft: scale(25),
-    fontSize: 15,
-    marginRight: scale(25)
+    fontSize: 16,
+    marginRight: scale(25),
+    textAlign: 'center',
+    color: 'black'
   }, 
   underline: {
     alignSelf: 'center',
@@ -69,6 +75,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 1
   },
   links: {
+    marginTop: '5%',
     flexDirection:'row',
     alignItems: 'center',
     justifyContent: 'center'
